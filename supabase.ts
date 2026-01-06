d10d81d52e98504969dd14b2aee081fdeff223a2
saimonalamshovo <146394919+saimonalamshovo@users.noreply.github.com>
2026-01-06 15:22:10 +0600

    Update supabase.ts

--- supabase.ts
+++ supabase.ts
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
   'https://xfvwklnfdiyqhtuhlvpi.supabase.co',
-  'PASTE_YOUR_ANON_KEY_HERE'
-);
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9....'
+)
