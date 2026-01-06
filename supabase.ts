abda2e15b8b362e519524e1b7a6c053e5a41462f
saimonalamshovo <146394919+saimonalamshovo@users.noreply.github.com>
2026-01-06 15:37:52 +0600

    Update supabase.ts

--- supabase.ts
+++ supabase.ts
@@ -1,11 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://xfvwklnfdiyqhtuhlvpi.supabase.co'
-
-const SUPABASE_ANON_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhmdndrbG5mZGl5cWh0dWhsdnBpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc2ODMxNTIsImV4cCI6MjA4MzI1OTE1Mn0.0gaHgnvZNZATcfwggiujCAe-VROFeqvJFkXfJAbpUnU'
-
 export const supabase = createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
+  'https://xfvwklnfdiyqhtuhlvpi.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhmdndrbG5mZGl5cWh0dWhsdnBpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc2ODMxNTIsImV4cCI6MjA4MzI1OTE1Mn0.0gaHgnvZNZATcfwggiujCAe-VROFeqvJFkXfJAbpUnU'
 )
